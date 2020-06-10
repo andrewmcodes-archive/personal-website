65bba880def6b2e9be9f17cee18112f401e39f12
Andrew Mason <andrewmcodes@protonmail.com>
2020-06-10 03:14:40 -0400

    refactor

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -9,6 +9,25 @@ const application = Application.start()
 const context = require.context('./controllers', true, /\.js$/)
 application.load(definitionsFromContext(context))
 
+import '../styles/markdown.scss'
 import '../styles/prism.scss'
 import Prism from './prism.js'
 Prism.highlightAll()
+
+/**
+ * Dropdown menu
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const menus = document.querySelectorAll('.navbar-burger')
+  const dropdowns = document.querySelectorAll('.navbar-menu')
+
+  if (menus.length && dropdowns.length) {
+    for (var i = 0; i < menus.length; i++) {
+      menus[i].addEventListener('click', function () {
+        for (var j = 0; j < dropdowns.length; j++) {
+          dropdowns[j].classList.toggle('hidden')
+        }
+      })
+    }
+  }
+})
--- src/index.md
+++ src/index.md
@@ -1,8 +1,9 @@
 ---
-# Feel free to add content and custom Front Matter to this file.
-
 layout: home
 ---
 
-<section>{% include home/hero.html %}</section>
-<section>{% include home/blog_section.html %}</section>
+{% render "home/hero" %}
+{% render "home/cta" %}
+{% render "home/blog", posts: site.posts %}
+{% render "home/involvement" %}
+{% render "shared/subscribe" %}
