e64fe83d9b1a62b05dd6caa2331c96ad1e75f8c5
Andrew Mason <andrewmcodes@protonmail.com>
2020-06-13 04:22:35 -0400

    feat: :sparkles: Add Swup transitions (#4)

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -2,6 +2,10 @@ import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import Prism from './prism.js'
 import '../styles/index.css'
+import Swup from 'swup'
+import SwupBodyClassPlugin from '@swup/body-class-plugin'
+import SwupScrollPlugin from '@swup/scroll-plugin'
+import SwupSlideTheme from '@swup/slide-theme'
 
 const application = Application.start()
 const context = require.context('./controllers', true, /\.js$/)
@@ -10,6 +14,17 @@ Prism.highlightAll()
 
 // Dropdown
 document.addEventListener('DOMContentLoaded', function () {
+  let mainEl = '#swup'
+  let containers = [mainEl, '#topnav']
+  new Swup({
+    containers: containers,
+    plugins: [
+      new SwupSlideTheme({ mainElement: mainEl }),
+      new SwupBodyClassPlugin(),
+      new SwupScrollPlugin({ animateScroll: false })
+    ]
+  })
+
   const menus = document.querySelectorAll('.navbar-burger')
   const dropdowns = document.querySelectorAll('.navbar-menu')
 
